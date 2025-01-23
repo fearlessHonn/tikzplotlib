cb10a27f3080bfcf382653a03d9504a1386c8634
Henning <37704045+fearlessHonn@users.noreply.github.com>
2025-01-23 13:58:49 +0100

    Fix matplotlib import error

--- src/tikzplotlib/_axes.py
+++ src/tikzplotlib/_axes.py
@@ -1,8 +1,6 @@
 import matplotlib as mpl
 import numpy as np
-from matplotlib.backends.backend_pgf import (
-    common_texification as mpl_common_texification,
-)
+from matplotlib.backends.backend_pgf import _tex_escape as mpl_common_texification
 
 from . import _color
 
